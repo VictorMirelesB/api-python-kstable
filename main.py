3c11d297987cc05dbe6b3e7a6df4619b3af6a10a
VictorMirelesB <vmirelesb@unjbg.edu.pe>
2021-12-20 02:47:28 -0500

    Update

--- main.py
+++ main.py
@@ -7,4 +7,19 @@ def get_kstable(GL: int, alfa: float):
   
   from scipy.stats import ksone
   x = ksone.ppf(1-alfa/2, GL)
-  return {"data": x, "GL": GL, "alfa": alfa, "author": "Victor Mireles"}
+
+  res = {
+    "data": x,
+    "Grado de Libertad": GL,
+    "Alfa": alfa,
+    "Authors" : [
+      {
+        "name": "Victor Mireles",
+      },
+      {
+        "name": "Carlos Ramos",
+      }
+    ]
+  }
+
+  return res
