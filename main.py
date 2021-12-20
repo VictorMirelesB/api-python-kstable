35609a91e03c28a2b6876e7688ba455da6120a3c
VictorMirelesB <vmirelesb@unjbg.edu.pe>
2021-12-20 03:07:55 -0500

    fix cors

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_header=["*"]
+)
 
 @app.get("/get-kstable")
 def get_kstable(GL: int, alfa: float):
